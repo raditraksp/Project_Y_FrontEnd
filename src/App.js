c3a783a5042a50c65e249d9e0ba1c077264c0d83
raditraksp <raditraksatria@gmail.com>
2020-06-07 12:51:14 +0700

    first jasaja

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          JASAJA <code>PROJECT</code> IN THE HOUSE YO.
         </p>
         <a
           className="App-link"
